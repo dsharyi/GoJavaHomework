0922b6961658c6b522f0088e6d706b034983c9f4
dsharyi <sharyy.90@mail.ru>
2017-02-02 13:41:57 +0200

    Initial commit

---
+++ src/main/java/com/sharyi_dmytro/practice/module01/task01/FifthClass.java
@@ -0,0 +1,7 @@
+package com.sharyi_dmytro.practice.module01.task01;
+
+public class FifthClass {
+    public static void main(String[] args) {
+        System.out.println("Мой первый класс на Java");
+    }
+}
