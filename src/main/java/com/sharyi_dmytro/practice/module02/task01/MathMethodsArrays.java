dc239373a9c35be09441e2289bba063e94db2c1c
dsharyi <sharyy.90@mail.ru>
2017-02-04 14:18:31 +0200

    Second Commit(04/02)

--- src/main/java/com/sharyi_dmytro/practice/module02/task01/MathMethodsArrays.java
+++ src/main/java/com/sharyi_dmytro/practice/module02/task01/MathMethodsArrays.java
@@ -5,139 +5,137 @@ import java.util.Arrays;
 
 public class MathMethodsArrays {
     public static void main(String[] args) {
-        int[] intArr = new int[10];
-        for (int i = 0; i < intArr.length; i++) {
-            intArr[i] = (int) (Math.random() * 10) - 5;
-        }
-        double[] doubleArr = new double[10];
-        for (int i = 0; i < doubleArr.length; i++) {
-            doubleArr[i] = (Math.random() * 10) - 5;
-        }
+        int[] intArr = {6, 9, -2, 8, 0, -4, -3, -10, 4, 2};
+        //int[] intArr = {-1, -3, -10};
+        double[] doubleArr = {6.2, 9.1, -2.5, 8.4, 0.9, -4.9, -3.4, -10.3, 4.5, 2.1};
 
         MathMethodsArrays mathMethodsArrays = new MathMethodsArrays();
-        System.out.println(Arrays.toString(intArr));
-        mathMethodsArrays.sum(intArr);
-        mathMethodsArrays.sum(doubleArr);
-        mathMethodsArrays.minMax(intArr);
-        mathMethodsArrays.minMax(doubleArr);
-        mathMethodsArrays.maxPositive(intArr);
-        mathMethodsArrays.maxPositive(doubleArr);
-        mathMethodsArrays.multiplication(intArr);
-        mathMethodsArrays.multiplication(doubleArr);
-        mathMethodsArrays.modulus(intArr);
-        mathMethodsArrays.modulus(doubleArr);
-        mathMethodsArrays.secondLargest(intArr);
-        mathMethodsArrays.secondLargest(doubleArr);
+        mathMethodsArrays.printSumOfArray(intArr);
+        mathMethodsArrays.printSumOfArray(doubleArr);
+        mathMethodsArrays.printMinMaxOfArray(intArr);
+        mathMethodsArrays.printMinMaxOfArray(doubleArr);
+        mathMethodsArrays.printMaxPositiveOfArray(intArr);
+        mathMethodsArrays.printMaxPositiveOfArray(doubleArr);
+        mathMethodsArrays.printMultiplicationOfArray(intArr);
+        mathMethodsArrays.printMultiplicationOfArray(doubleArr);
+        mathMethodsArrays.printModulusFirstAndLastElementsOfArray(intArr);
+        mathMethodsArrays.printModulusFirstAndLastElementsOfArray(doubleArr);
+        mathMethodsArrays.printSecondLargestElementOfArray(intArr);
+        mathMethodsArrays.printSecondLargestElementOfArray(doubleArr);
 
     }
 
-    public void sum(int[] intArr) {
-        int sum = 0;
+    public void printSumOfArray(int[] intArr) {
+        int resultSumOfArrayInt = 0;
         for (int j = 0; j < intArr.length; j++) {
-            sum += intArr[j];
+            resultSumOfArrayInt += intArr[j];
         }
-        System.out.println("Суммма массива = " + sum);
+        System.out.println("Суммма массива = " + resultSumOfArrayInt);
     }
 
-    public void sum(double[] doubleArr) {
-        double sum = 0;
+    public void printSumOfArray(double[] doubleArr) {
+        double resultSumOfArrayDouble = 0;
         for (int j = 0; j < doubleArr.length; j++) {
-            sum += doubleArr[j];
+            resultSumOfArrayDouble += doubleArr[j];
         }
-        System.out.println("Суммма массива = " + sum);
+        System.out.println("Суммма массива = " + resultSumOfArrayDouble);
     }
 
-    public void minMax(int[] intArr) {
+    public void printMinMaxOfArray(int[] intArr) {
         Arrays.sort(intArr);
         System.out.println("Минимальное значение массива = " + intArr[0]);
         System.out.println("Максимальное значение массива = " + intArr[intArr.length - 1]);
 
     }
 
-    public void minMax(double[] doubleArr) {
+    public void printMinMaxOfArray(double[] doubleArr) {
         Arrays.sort(doubleArr);
         System.out.println("Минимальное значение массива = " + doubleArr[0]);
         System.out.println("Максимальное значение массива = " + doubleArr[doubleArr.length - 1]);
 
     }
 
-    public void maxPositive(int[] intArr) {
-        int[] positiveArr = new int[intArr.length];
-        for (int i = 0; i < intArr.length; i++) {
-            if (intArr[i] > 0) {
-                positiveArr[i] = intArr[i];
-            }
+    public void printMaxPositiveOfArray(int[] intArr) {
+        Arrays.sort(intArr);
+        if (intArr[intArr.length - 1] <= 0) {
+            System.out.println("В этом массиве нету положительных элементов");
+        } else {
+            System.out.println("Максимальное положительное значение = " + intArr[intArr.length - 1]);
+
         }
-        System.out.println("Максимальное положительное значение = " + positiveArr[positiveArr.length - 1]);
 
     }
 
-    public void maxPositive(double[] doubleArr) {
-        double[] positiveArr = new double[doubleArr.length];
-        for (int i = 0; i < doubleArr.length; i++) {
-            if (doubleArr[i] > 0) {
-                positiveArr[i] = doubleArr[i];
-            }
-        }
-        System.out.println("Максимальное положительное значение = " + positiveArr[positiveArr.length - 1]);
+    public void printMaxPositiveOfArray(double[] doubleArr) {
+        Arrays.sort(doubleArr);
+        if (doubleArr[doubleArr.length - 1] <= 0) {
+            System.out.println("В этом массиве нету положительных элементов");
+        } else {
+            System.out.println("Максимальное положительное значение = " + doubleArr[doubleArr.length - 1]);
 
+        }
     }
 
-    public void multiplication(int[] intArr) {
-        int mult = 1;
+    public void printMultiplicationOfArray(int[] intArr) {
+        int resultMultOfArrayInt = 1;
         for (int j = 0; j < intArr.length; j++) {
-            mult *= intArr[j];
+            resultMultOfArrayInt *= intArr[j];
         }
-        System.out.println("Произведение всех элементов массива = " + mult);
+        System.out.println("Произведение всех элементов массива = " + resultMultOfArrayInt);
     }
 
-    public void multiplication(double[] doubleArr) {
-        double mult = 1;
+    public void printMultiplicationOfArray(double[] doubleArr) {
+        double resultMultOfArrayDouble = 1;
         for (int j = 0; j < doubleArr.length; j++) {
-            mult *= doubleArr[j];
+            resultMultOfArrayDouble *= doubleArr[j];
         }
-        System.out.println("Произведение всех элементов массива = " + mult);
+        System.out.println("Произведение всех элементов массива = " + resultMultOfArrayDouble);
     }
 
-    public void modulus(int[] intArr) {
-        int[] modulusArr = new int[intArr.length];
-        for (int i = 0; i < intArr.length; i++) {
-            if (intArr[i] < 0) {
-                intArr[i] *= -1;
-                modulusArr[i] = intArr[i];
-                intArr[i] *= -1;
-            } else
-                modulusArr[i] = intArr[i];
+    public void printModulusFirstAndLastElementsOfArray(int[] intArr) {
+
+        if (intArr[0] < 0) {
+            intArr[0] *= -1;
+        } else {
+            intArr[0] = intArr[0];
 
         }
-        System.out.println("Модуль первого элемента массива = " + modulusArr[0]);
-        System.out.println("Модуль последнего элемента массива = " + modulusArr[modulusArr.length - 1]);
-    }
+        if (intArr[intArr.length - 1] < 0) {
+            intArr[intArr.length - 1] *= -1;
+        } else {
+            intArr[intArr.length - 1] = intArr[intArr.length - 1];
 
-    public void modulus(double[] doubleArr) {
-        double[] modulusDouble = new double[doubleArr.length];
-        for (int i = 0; i < doubleArr.length; i++) {
-            if (doubleArr[i] < 0) {
-                doubleArr[i] *= -1;
-                modulusDouble[i] = doubleArr[i];
-                doubleArr[i] *= -1;
-            } else
-                modulusDouble[i] = doubleArr[i];
         }
+        System.out.println("Модуль первого элемента массива = " + intArr[0]);
+        System.out.println("Модуль последнего элемента массива = " + intArr[intArr.length - 1]);
+    }
 
-        System.out.println("Модуль первого элемента массива = " + modulusDouble[0]);
-        System.out.println("Модуль последнего элемента массива = " + modulusDouble[doubleArr.length - 1]);
+    public void printModulusFirstAndLastElementsOfArray(double[] doubleArr) {
+        if (doubleArr[0] < 0) {
+            doubleArr[0] *= -1;
+        } else {
+            doubleArr[0] = doubleArr[0];
+
+        }
+        if (doubleArr[doubleArr.length - 1] < 0) {
+            doubleArr[doubleArr.length - 1] *= -1;
+        } else {
+            doubleArr[doubleArr.length - 1] = doubleArr[doubleArr.length - 1];
+
+        }
+        System.out.println("Модуль первого элемента массива = " + doubleArr[0]);
+        System.out.println("Модуль последнего элемента массива = " + doubleArr[doubleArr.length - 1]);
     }
 
 
-    public void secondLargest(int[] intArr) {
+    public void printSecondLargestElementOfArray(int[] intArr) {
         Arrays.sort(intArr);
         System.out.println("Второе максимальное значение массива = " + intArr[intArr.length - 2]);
 
 
     }
 
-    public void secondLargest(double[] doubleArr) {
+    public void printSecondLargestElementOfArray(double[] doubleArr) {
         Arrays.sort(doubleArr);
         System.out.println("Второе максимальное значение массива = " + doubleArr[doubleArr.length - 2]);
 
