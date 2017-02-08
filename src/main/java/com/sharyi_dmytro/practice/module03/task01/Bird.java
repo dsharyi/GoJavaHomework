7ce8f7bf0ad91e51cd372151a5589e791451f666
dsharyi <sharyy.90@mail.ru>
2017-02-08 18:20:55 +0200

    Commit(08/02)

---
+++ src/main/java/com/sharyi_dmytro/practice/module03/task01/Bird.java
@@ -0,0 +1,13 @@
+package com.sharyi_dmytro.practice.module03.task01;
+
+
+public class Bird {
+
+    String sing(String song){
+        System.out.println(song);
+        return song;
+    }
+
+
+
+}
