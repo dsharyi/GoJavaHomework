b501d688959e51a4125f927380dc6601b5347c1a
dsharyi <sharyy.90@mail.ru>
2017-02-11 18:37:30 +0200

    Commit(11/02)

--- src/main/java/com/sharyi_dmytro/practice/module02/task02/BankOperation.java
+++ src/main/java/com/sharyi_dmytro/practice/module02/task02/BankOperation.java
@@ -2,19 +2,17 @@ package com.sharyi_dmytro.practice.module02.task02;
 
 
 public class BankOperation {
-    int balance = 100;
-    int withdrawal = 10;
-    //int withdrawal = 99;
+
     public static final double PERCENT_OF_COMMISION = 0.05;
 
     public static void main(String[] args) {
         BankOperation bankOperation = new BankOperation();
-        bankOperation.conditionOfBalance();
+        bankOperation.conditionOfBalance(100,10);
 
 
     }
 
-    public void conditionOfBalance() {
+    public void conditionOfBalance(int balance, int withdrawal) {
         double commision = withdrawal * PERCENT_OF_COMMISION;
         double balanceAfterWithdrawal = balance - withdrawal - commision;
         if (balanceAfterWithdrawal >= 0) {
