d92d6209f8fb008bc3e77a2bf94270269765d6f9
dsharyi <sharyy.90@mail.ru>
2017-02-13 18:50:43 +0200

    Commit(13/02)

--- src/main/java/com/sharyi_dmytro/practice/module03/task01/Bird.java
+++ src/main/java/com/sharyi_dmytro/practice/module03/task01/Bird.java
@@ -3,11 +3,51 @@ package com.sharyi_dmytro.practice.module03.task01;
 
 public class Bird {
 
-    String sing(String song){
-        System.out.println(song);
-        return song;
+    public static void main(String[] args) {
+
+        Bird birdVoody = new Bird("Voody");
+        birdVoody.sing();
+        //Bird bird = new Bird();
+        //bird.sing();
+
+
     }
 
 
+    private static final String[] SONG = new String[]{"I am walking",
+            "I am flying",
+            "I am singing",
+            "I am Bird"};
 
+
+    private String name;
+
+    public Bird(String name) {
+        this.name = name;
+    }
+
+    public Bird() {
+    }
+
+    @Override
+    public String toString() {
+        return "I am walking \n" +
+                "I am flying \n" +
+                "I am singing \n" +
+                "I am Bird";
+    }
+
+
+    void sing() {
+        if (name == null) {
+            System.out.println(toString());
+
+
+        } else {
+            System.out.println("I am " + name + "\n" + toString());
+
+        }
+
+
+    }
 }
