7fc7c173e6755d8b7a2acdb3572656991d35adfb
dsharyi <sharyy.90@mail.ru>
2017-02-13 17:22:16 +0200

    Commit(13/02)

--- src/main/java/com/sharyi_dmytro/practice/module02/task04/FundBalance.java
+++ src/main/java/com/sharyi_dmytro/practice/module02/task04/FundBalance.java
@@ -2,38 +2,45 @@ package com.sharyi_dmytro.practice.module02.task04;
 
 
 public class FundBalance {
-    int[] balances = {1200, 250, 2000, 500, 3200};
-    String[] ownerNames = {"Jane", "Ann", "Jack", "Oww", "Lane"};
+    private int[] balances;
+    private String[] ownerNames;
 
 
     public static void main(String[] args) {
-        FundBalance fundBalance = new FundBalance();
-        fundBalance.FundingBalance("Oww",100);
 
+        FundBalance fundBalance = new FundBalance();
+        fundBalance.setBalances(new int[]{1200, 250, 2000, 500, 3200});
+        fundBalance.setOwnerNames(new String[]{"Jane", "Ann", "Jack", "Oww", "Lane"});
+        fundBalance.FundingBalance("Jack", 100);
+
+    }
+
+    public void setBalances(int[] balances) {
+        this.balances = balances;
+    }
+
+    public void setOwnerNames(String[] ownerNames) {
+        this.ownerNames = ownerNames;
+    }
+
+    public int[] getBalances() {
+
+        return balances;
+    }
+
+    public String[] getOwnerNames() {
+        return ownerNames;
     }
 
     public void FundingBalance(String ownerName, double withdrawal) {
-        if (ownerName == "Jane") {
-            double balanceAfterFundingJane = balances[0] + withdrawal;
-            System.out.println(ownerName + " Balance after funding = " + balanceAfterFundingJane);
+        for (int i = 0; i < getOwnerNames().length; i++) {
+            if (ownerName.equals(getOwnerNames()[i])) {
+                double balanceAfterFunding = getBalances()[i] + withdrawal;
+                System.out.println(ownerName + " Balance after funding = " + balanceAfterFunding);
+                break;
 
-        }
-        if (ownerName == "Ann") {
-            double balanceAfterFundingAnn = balances[1] + withdrawal;
-            System.out.println(ownerName + " Balance after funding = " + balanceAfterFundingAnn);
-        }
-        if (ownerName == "Jack") {
-            double balanceAfterFundingJack = balances[2] + withdrawal;
-            System.out.println(ownerName + " Balance after funding = " + balanceAfterFundingJack);
-        }
-        if (ownerName == "Oww") {
-            double balanceAfterFundingOww = balances[3] + withdrawal;
-            System.out.println(ownerName + " Balance after funding = " + balanceAfterFundingOww);
-        }
-        if (ownerName == "Lane") {
-            double balanceAfterFundingLane = balances[4] + withdrawal;
-            System.out.println(ownerName + " Balance after funding = " + balanceAfterFundingLane);
-
+            }
         }
     }
 }
+
