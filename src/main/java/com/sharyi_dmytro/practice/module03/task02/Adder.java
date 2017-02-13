e7ae184628c6691a49b3a30801756c1918e8b590
dsharyi <sharyy.90@mail.ru>
2017-02-13 18:56:28 +0200

    Commit(13/02)

--- src/main/java/com/sharyi_dmytro/practice/module03/task02/Adder.java
+++ src/main/java/com/sharyi_dmytro/practice/module03/task02/Adder.java
@@ -16,8 +16,10 @@ public class Adder extends Arithmetic {
     /*public static void main(String[] args) {
         Adder math = new Adder();
 
-        System.out.println(math.add(5,6));
-        System.out.println(math.check(7,8));*/
+        System.out.println(math.add(5, 6));
+        System.out.println(math.check(8, 8));
+        */
 
 
-}
+    }
+
